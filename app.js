58cfd36ce4ffdc36b9b33b48c9c30535b370024f
Ahmed0rashed <ahmedmohamedrashed236@gmail.com>
2025-01-07 23:18:46 +0200

    solve problem

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-const userRouter = require('./routes/user.routes');
+
 const pationtRouter = require('./routes/pationt.routes');
 const app = express();
 
@@ -13,6 +13,6 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/users', userRouter);
+
 app.use("/api/patients", pationtRouter);
 module.exports = app;
