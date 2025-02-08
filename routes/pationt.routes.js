d6d7ec7d350ce8e7f3a4198a1c82e5213ff42704
fady riad <91222856+FadyRiad@users.noreply.github.com>
2025-02-09 01:21:56 +0200

    patients route
    get patient statistics from the database

--- routes/pationt.routes.js
+++ routes/pationt.routes.js
@@ -6,6 +6,8 @@ const userController = require('../controllers/pationt.controller');
 
 router.post('/addPatient', userController.addPatient);
 router.get('/getPatients', userController.getAllPatients);
+router.get("/getPatientStatistics", userController.getPatientStatistics);// get patient statistics from the database
+
 
 
 
