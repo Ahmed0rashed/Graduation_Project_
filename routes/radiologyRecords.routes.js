a402fabb5fbb431b24eee4d90cd58ad5e56b25c6
Ahmed0rashed <ahmedmohamedrashed236@gmail.com>
2025-02-12 18:30:22 +0200

    add getRecordsByRediologyId API

--- routes/radiologyRecords.routes.js
+++ routes/radiologyRecords.routes.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const authController = require('../controllers/RadiologyRecored.controller');
  
 router.post('/addRecord', authController.addRecord);
-router.get('/getRecordsByCenterId/:id', authController.getRecordsByCenterId);
 router.get('/getRecordById/:id', authController.getRecordById);
 router.delete('/deleteRecordById/:id', authController.deleteRecordById);
-router.delete('/getNumberOfRecords/:id', authController.getNumberOfRecords);
-
+router.get('/getNumberOfRecordsByCenterId/:id', authController.getNumberOfRecords);
+router.get('/getRecordsByCenterId/:id', authController.getRecordsByCenterId);
+router.get('/getRecordsByRediologyId/:id', authController.getRecordsByRediologyId);
 
 
 
