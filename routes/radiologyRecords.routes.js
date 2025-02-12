36d6679c861587a3b823dc768e455dc2161fdc38
Ahmed0rashed <ahmedmohamedrashed236@gmail.com>
2025-02-12 04:28:49 +0200

    edit radiology record routes

--- routes/radiologyRecords.routes.js
+++ routes/radiologyRecords.routes.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const authController = require('../controllers/RadiologyRecored.controller');
  
 router.post('/addRecord', authController.addRecord);
+router.get('/getRecordsByCenterId/:id', authController.getRecordsByCenterId);
+router.get('/getRecordById/:id', authController.getRecordById);
+router.delete('/deleteRecordById/:id', authController.deleteRecordById);
+router.delete('/getNumberOfRecords/:id', authController.getNumberOfRecords);
+
 
 
 
