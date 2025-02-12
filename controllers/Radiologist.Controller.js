655bbc5c7cfe8ac6e5b4d37225872a1d360f784f
Ahmed0rashed <ahmedmohamedrashed236@gmail.com>
2025-02-12 22:23:57 +0200

    add getRadiologistById api

--- controllers/Radiologist.Controller.js
+++ controllers/Radiologist.Controller.js
@@ -1,62 +1,24 @@
-const Radiologist = require('../models/Radiologists.Model');
+const express = require("express");
+const mongoose = require("mongoose");
+const AIReport = require("../models/Radiologists.Model"); 
+
+const router = express.Router();
 
 
 
-class RadiologistController{
-     //Get all rodiologists 
-     async getAllRadiologists(req, res) {
-          try {
-               const {
-                    specialization,
-                    status,
-                    available,
-                    search,
-                    limit = 10,
-                    page = 1
-                  } = req.body;
+exports.getRadiologistById = async (req, res) => {
+  try {
+    const radiologist = await AIReport.findById(req.params.id);
+    if (!radiologist) return res.status(404).json({ message: "Radiologist not found" });
 
-                  let query = {};
-
-                  if (specialization) query.specialization = specialization;
-                  if (status) query.status = status;
-                  if (search) query.$text = {$text: search};
-                  if (available === 'true') {
-                    const currentTime = new Date().toTimeString().slice(0, 5);
-                    query.status = 'Active';
-                    query['availableHours.start'] = { $lte: currentTime };
-                    query['availableHours.end'] = { $gte: currentTime };
-                  }
-                  const skip = (page - 1) * limit;
-
-                  const [radiologists, total] = await Promise.all([
-                    Radiologist.find(query)
-                      .populate('centerId', 'name address')
-                      .skip(skip)
-                      .limit(parseInt(limit))
-                      .select('-passwordHash'),
-                    Radiologist.countDocuments(query)
-                  ]);
-
-                  res.json({
-                    radiologists,
-                    pagination: {
-                      total,
-                      page: parseInt(page),
-                      pages: Math.ceil(total / limit)
-                    }
-                  });
-                } catch (error) {
-                  res.status(500).json({ message: error.message });
-                }
-              }
+    res.status(200).json(radiologist);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 
 
-      // Get single radiologist by ID
-     
-              
 
 
-}
-
-module.exports = new RadiologistController();
+module.exports = exports;
