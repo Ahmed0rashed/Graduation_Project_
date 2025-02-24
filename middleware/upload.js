546c5d95a883117a6254d5a59017c1ef49df38f4
Ahmed0rashed <ahmedmohamedrashed236@gmail.com>
2025-02-24 19:44:22 +0200

    solve problem

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,16 +1,14 @@
 const multer = require('multer');
 
-// تحديد مكان تخزين الملفات
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/');  // تأكد من وجود مجلد "uploads"
+    cb(null, 'uploads/');  
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + '-' + file.originalname);
   }
 });
 
-// تعريف multer
 const upload = multer({ storage: storage });
 
 module.exports = upload;
