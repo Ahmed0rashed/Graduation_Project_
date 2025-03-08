89d57ee947ab5b37254b839530b6bb96430751f2
Ahmed0rashed <ahmedmohamedrashed236@gmail.com>
2025-03-08 22:44:32 +0200

    add upload image and get image to radiologist

--- controllers/Radiologist.Controller.js
+++ controllers/Radiologist.Controller.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Radiologist = require("../models/Radiologists.Model"); 
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: "dncawa23w",
+  api_key: "451913596668632",
+  api_secret: "KboaQ-CpKdNpD0oJ0JvAagR3N_4",
+});
 
 const router = express.Router();
 
@@ -29,8 +36,58 @@ exports.editRadiologist = async (req, res) => {
   }
 };
 
+exports.uploadImage = async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).json({ message: "No file uploaded" });
+    }
+
+    const center = await Radiologist.findById(req.params.id);
+    if (!center) {
+      return res.status(404).json({ message: "Radiologist not found" });
+    }
+
+    const result = await new Promise((resolve, reject) => {
+      const uploadStream = cloudinary.uploader.upload_stream(
+        { folder: "Radiologist" },
+        (error, result) => {
+          if (error) return reject(error);
+          resolve(result);
+        }
+      );
+      uploadStream.end(req.file.buffer);
+    });
 
 
+    center.image = result.secure_url;
+    await center.save();
+
+    res.status(200).json({
+      message: "Image uploaded successfully",
+      url: result.secure_url,
+    });
+  } catch (error) {
+    console.error("Server error:", error);
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+exports.getImage = async (req, res) => {
+  try {
+    const center = await Radiologist.findById(req.params.id);
+    if (!center) {
+      return res.status(404).json({ message: "Center not found" });
+    }
+
+    res.status(200).json({
+      message: "Image retrieved successfully",
+      imageUrl: center.image,
+    });
+  } catch (error) {
+    console.error("Server error:", error);
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
 
 
 module.exports = exports;
--- routes/Radiologist.Routes.js
+++ routes/Radiologist.Routes.js
@@ -1,10 +1,18 @@
+
+
 const express = require('express');
 const router = express.Router();
+const multer = require("multer");
 const RadiologistController = require('../controllers/Radiologist.Controller');
-
+const { uploadImage } = require("../controllers/Radiologist.Controller");
+const uploadImag = require("../controllers/Radiologist.Controller");
+const storage = multer.memoryStorage();
+const fileUpload = multer({ storage: storage });
 
 router.get('/getRadiologistById/:id', RadiologistController.getRadiologistById);
 
 router.get('/editRadiologist/:id', RadiologistController.editRadiologist);
 
+router.post("/upload/:id", fileUpload.single("image"), uploadImage);
+router.get("/getImage/:id", uploadImag.getImage);
 module.exports = router;
