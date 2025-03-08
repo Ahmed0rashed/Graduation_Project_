eea464a1c707543aedd996f6421da264c5006012
fady riad <91222856+FadyRiad@users.noreply.github.com>
2025-03-08 16:26:45 +0200

    add deleteRadiologist &  updateRadiologist
    Update radiologist by ID
    and Delete a radiologist by ID in class RadiologistController

--- routes/Radiologist.Routes.js
+++ routes/Radiologist.Routes.js
@@ -4,8 +4,8 @@ const RadiologistController = require('../controllers/Radiologist.Controller');
 
 
 router.get('/getRadiologistById/:id', RadiologistController.getRadiologistById);
-
-
+router.delete('/deleteRadiologistById/:id', RadiologistController.deleteRadiologist);
+router.put('/updateRadiologist/:id', RadiologistController.updateRadiologist);
 router.patch('/editRadiologist/:id', RadiologistController.editRadiologist);
 
 module.exports = router;
