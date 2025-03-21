5df742c7558f06074025e68998fe8d40d540b91e
Ahmed0rashed <ahmedmohamedrashed236@gmail.com>
2025-03-21 20:15:41 +0200

    solve problem

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
-const dotenv = require("dotenv");
+require("dotenv").config({ path: "./config.env" });
 const express = require("express");
 const connectDB = require("./config/db.config");
 const http = require("http");
 const app = require("./app");
 const { initializeSocket } = require("./middleware/socketManager");
 
-dotenv.config({ path: "./config.env" });
-
+// إنشاء الخادم
 const server = http.createServer(app);
 const io = initializeSocket(server);
 
+// الاتصال بقاعدة البيانات
 connectDB();
 
+// تشغيل الخادم على المنفذ المحدد
 const port = process.env.PORT || 8000;
 server.listen(port, () => console.log(`App running on port ${port}`));
 
-module.exports = { io };
+// ✅ تصدير الخادم بشكل افتراضي ليكون متوافقًا مع Vercel
+module.exports = server;
