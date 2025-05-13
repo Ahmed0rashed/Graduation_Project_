a70bac31fa2f2480ae1e5b0f168541ea9565b934
Ahmed0rashed <ahmedmohamedrashed236@gmail.com>
2025-05-13 16:44:10 +0300

    add notifcation to comment

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -36,90 +36,103 @@ const sendNotification = async (
   }
 };
 exports.addcommmet = async (req, res) => {
-    try {
-      const { recordId, userType, Comments } = req.body;
-  
-      if (!recordId || !userType || !Comments) {
+  try {
+    const { recordId, userType, Comments } = req.body;
+
+    if (!recordId || !userType || !Comments) {
+      return res
+        .status(400)
+        .json({ message: "recordId, userType, and Comments are required" });
+    }
+
+    const radiologyRecord = await RadiologyRecord.findById(recordId);
+    if (!radiologyRecord) {
+      return res.status(404).json({ message: "Record not found" });
+    }
+
+    let Name, Image, userId;
+
+    if (userType === "Radiologist") {
+      const radiologistId = radiologyRecord.radiologistId;
+      if (!radiologistId) {
         return res
-          .status(400)
-          .json({ message: "recordId, userType, and Comments are required" });
+          .status(404)
+          .json({ message: "Radiologist ID not found in record" });
       }
-  
-      const radiologyRecord = await RadiologyRecord.findById(recordId);
-      if (!radiologyRecord) {
-        return res.status(404).json({ message: "Record not found" });
+
+      const radiologist = await Radiologist.findById(radiologistId);
+      if (!radiologist) {
+        return res.status(404).json({ message: "Radiologist not found" });
       }
-  
-      let Name, Image, userId;
-  
-      if (userType === "Radiologist") {
-        const radiologistId = radiologyRecord.radiologistId;
-        if (!radiologistId) {
-          return res
-            .status(404)
-            .json({ message: "Radiologist ID not found in record" });
-        }
-  
-        const radiologist = await Radiologist.findById(radiologistId);
-        if (!radiologist) {
-          return res.status(404).json({ message: "Radiologist not found" });
-        }
-  
-        Name = `${radiologist.firstName} ${radiologist.lastName}`;
-        Image = radiologist.image;
-        userId = radiologistId;
-      } else {
-        const centerId = radiologyRecord.centerId;
-        if (!centerId) {
-          return res
-            .status(404)
-            .json({ message: "Center ID not found in record" });
-        }
-  
-        const center = await RadiologyCenter.findById(centerId);
-        if (!center) {
-          return res.status(404).json({ message: "Center not found" });
-        }
-  
-        Name = center.centerName;
-        Image = center.image;
-        userId = centerId;
+
+      Name = `${radiologist.firstName} ${radiologist.lastName}`;
+      Image = radiologist.image;
+      userId = radiologistId;
+    } else {
+      const centerId = radiologyRecord.centerId;
+      if (!centerId) {
+        return res
+          .status(404)
+          .json({ message: "Center ID not found in record" });
       }
-  
-      // ✅ Create a new Comment document
-      const newComment = new Comment({
-        name: Name,
-        recordId: recordId,
-        image: Image,
-        userType: userType,
-        userId: userId,
-        dicom_Comment: [Comments], // If single comment string, wrap in array
-      });
-  
-      await newComment.save();
-  
-      res.status(200).json({ message: "Comment added successfully", comment: newComment });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
+
+      const center = await RadiologyCenter.findById(centerId);
+      if (!center) {
+        return res.status(404).json({ message: "Center not found" });
+      }
+
+      Name = center.centerName;
+      Image = center.image;
+      userId = centerId;
+      const notificationResult = await sendNotification(
+        radiologyRecord.radiologistId,
+        "Radiologist",
+        center.centerName,
+        "New comment on your record",
+        center.image,
+        center.centerName,
+        "study"
+      );
+     
     }
-  };
-  
-  exports.getAllCommentsByRecordId = async (req, res) => {
-    try {
-      const { recordId } = req.params;
-      if (!recordId) {
-        return res.status(400).json({ message: "Record ID is required" });
-      }
-  
-      const comments = await Comment.find({ recordId }).sort({ createdAt: -1 });
-      if (!comments.length) {
-        return res.status(404).json({ message: "No comments found for this record" });
-      }
-  
-      res.status(200).json(comments);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
+
+    
+    const newComment = new Comment({
+      name: Name,
+      recordId: recordId,
+      image: Image,
+      userType: userType,
+      userId: userId,
+      dicom_Comment: [Comments], 
+    });
+
+    await newComment.save();
+
+    res
+      .status(200)
+      .json({ message: "Comment added successfully", comment: newComment });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.getAllCommentsByRecordId = async (req, res) => {
+  try {
+    const { recordId } = req.params;
+    if (!recordId) {
+      return res.status(400).json({ message: "Record ID is required" });
     }
-  };
-  module.exports = exports;
-  
+
+    const comments = await Comment.find({ recordId }).sort({ createdAt: -1 });
+    if (!comments.length) {
+      return res
+        .status(404)
+        .json({ message: "No comments found for this record" });
+    }
+
+    res.status(200).json(comments);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+module.exports = exports;
